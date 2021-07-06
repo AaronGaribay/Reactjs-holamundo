2f75f11b8fcc8c611718a091d6424379cc1d6c26
AaronGaribay <al14760427@ite.edu.mx>
2021-07-06 00:31:54 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HolaMundo from './components/HolaMundo';
+import Button from './components/Button'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Button text="Click 6" />
   </React.StrictMode>,
   document.getElementById('root')
 );
